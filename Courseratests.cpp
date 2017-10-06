f8ddd0c6ce2e44c0b835f0403e1ef7c2459dbebd
DenisKalou <32569581+DenisKalou@users.noreply.github.com>
2017-10-06 18:19:35 +0300

    Update Courseratests.cpp

--- Courseratests.cpp
+++ Courseratests.cpp
@@ -14,31 +14,35 @@ struct Student { // structure of student data
 };
 
 int main() {
-	string path = "input.txt"; // path to file
-	ifstream input(path);
-	int n, m;
-	input >> n; // students quantity
+//	string path = "input.txt"; // path to file
+//	ifstream input(path);
+	unsigned int n, m;
+	string first_name, second_name;
+	int day, month, year;
+	cin >> n; // students quantity
 	vector<Student> students(n); // students data
-	for (int i = 0; i < n; ++i){ // reading input students data from file
-		input >> students[i].first_name;
-		input >> students[i].second_name;
-		input >> students[i].day;
-		input >> students[i].month;
-		input >> students[i].year;
+	for (size_t i = 0; i < n; ++i){ // reading input students data from file
+		cin >> first_name >> second_name >> day >> month >> year;
+		if(first_name.size() > 0 && first_name.size() < 16
+				&& second_name.size() > 0 && second_name.size() < 16
+				&& day >= 0 && day <= 1000000000
+				&& month >= 0 && month <= 1000000000
+				&& year >= 0 && year <= 1000000000){
+			students[i] = { first_name, second_name, day, month, year };
+		}
 	}
-	input >> m; // operational command quantity
-	for (int i = 0; i < m; ++i){ // operational command usage and result
+	cin >> m; // operational command quantity
+	for (size_t i = 0; i < m; ++i){ // operational command usage and result
 		string operational_command;
-		int code;
-		input >> operational_command // reading operational command name
-		>> code; // reading operational command code
-		--code;
-		if (operational_command == "name"){
-			cout << students[code].first_name
+		unsigned int code;
+		cin >> operational_command >> code;// reading operational command name
+		code--;
+		if (operational_command == "name" && code > 0 && code < students.size()){
+			cout << students[code - 1].first_name
 					<<  " " << students[code].second_name
 					<< endl;
-		} else if (operational_command == "date"){
-			cout << students[code].day
+		} else if (operational_command == "date" && code > 0 && code < students.size()){
+			cout << students[code - 1].day
 					<< "." << students[code].month
 					<< "." << students[code].year
 					<< endl;
@@ -48,3 +52,4 @@ int main() {
 	}
 	return 0;
 }
+
